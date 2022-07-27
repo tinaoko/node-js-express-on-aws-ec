683ce7341ec6d26e2c03b497f8e9bf19c95c7887
tinaoko <109707153+tinaoko@users.noreply.github.com>
2022-07-26 22:02:47 -0500

    Update securitygroup.tf

--- securitygroup.tf
+++ securitygroup.tf
@@ -1,10 +1,9 @@
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
+resource "aws" {
+  name        = "jenkins practice" 
+  description = "deploy a security group in aws"
 
   ingress {
-    description      = "TLS from VPC"
+    description      = "security group deployment"
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
@@ -16,7 +15,6 @@ resource "aws_security_group" "allow_tls" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
