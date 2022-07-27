99ae8717a080eb1f8419de9c0b64797a5a51b681
tinaoko <109707153+tinaoko@users.noreply.github.com>
2022-07-26 20:58:49 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,9 @@
-
+variable "access_key" {
+  default = "AKIAQHZ23YUOR2BZT6WA"
+}
+variable "secret_key" {
+  default = "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"
+}
+variable "region" {
+  default = "us-east-2"
+}
